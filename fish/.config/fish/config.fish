7017ffacb877eae471fc5a1405d20b0efb261988
Angel Leon <alc111@outlook.es>
2023-08-23 20:46:18 +0200

    Cambio la config de fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,15 +3,23 @@ if status is-interactive
 	function ls
 		exa --icons $argv
 	end
+	function ssh
+		TERM=xterm-256color /usr/bin/ssh $argv
+	end
+	function vim
+		nvim $argv
+	end
 	##VARIABLES
 	set fish_greeting
 	set -x EDITOR "nvim"
 	set -x VISUAL "nvim"
 	##STARTUP COMMANDS
-	fm6000 -n -c cyan -f ~/.config/pixelart/space_invader.txt
+	#fm6000 -n -c cyan -f ~/.config/pixelart/space_invader.txt
+	pfetch
 	##INIT STARSHIP PROMPT
 	starship init fish | source
 	##PATH
 	fish_add_path /home/alc111/.cargo/bin
 	fish_add_path /home/alc111/.deno/bin
+	fish_add_path /home/alc111/go/bin
 end
